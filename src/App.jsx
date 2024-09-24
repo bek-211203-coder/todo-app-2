135db7a8c41932fe47b6f9af1fd123d28a4bd769
avazbek <avazbek@gmail.com>
2024-09-24 13:26:05 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Form from './companent/Form'
+
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/companent/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <div>
+            <input type="text" placeholder='malumot yoz' className=''/>
+            <button>+</button>
+        </div>
+    </div>
+  )
+}
+
+export default Form
